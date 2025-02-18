1cc391212bada9d5aa60584a19ae866eee28dc3b
Kent <kent.yuan@Gmail.com>
2025-02-18 23:55:48 +0100

    Day 01 ✅

--- src/Day01.kt
+++ src/Day01.kt
@@ -6,18 +6,26 @@ fun main() {
 
     val input = readInput(today)
     val testInput = readTestInput(today)
+    val testInputPart2 = readTestInput("$today-part2")
 
     fun part1(input: List<String>): Int {
-        return 0
+        val digits = input.single().toCharArray()
+        return digits.mapIndexed { i, n ->
+            val nextIdx = if (i == digits.lastIndex) 0 else i + 1
+            if (n == digits[nextIdx]) n.digitToInt() else 0
+        }.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val digits = input.single().toCharArray()
+        val steps = digits.size / 2
+        val dd = digits + digits
+        return digits.mapIndexed { i, n -> if (n == dd[i + steps]) n.digitToInt() else 0 }.sum()
     }
 
-    chkTestInput(Part1, testInput, 0) { part1(it) }
+    chkTestInput(Part1, testInput, 4) { part1(it) }
     solve(Part1, input) { part1(it) }
 
-    chkTestInput(Part2, testInput, 0) { part2(it) }
+    chkTestInput(Part2, testInputPart2, 4) { part2(it) }
     solve(Part2, input) { part2(it) }
 }
