4fb58b6c015ca3d5277d08f7856a0c2b3b1d60c0
Kent <kent.yuan@Gmail.com>
2025-03-24 23:33:29 +0100

    Day 09 ✅

--- src/Day09.kt
+++ src/Day09.kt
@@ -7,17 +7,34 @@ fun main() {
     val input = readInput(today)
     val testInput = readTestInput(today)
 
-    fun part1(input: List<String>): Int {
-        return 0
-    }
+    fun part1(input: List<String>) = Day09(input.single()).groupScore()
 
-    fun part2(input: List<String>): Int {
-        return 0
-    }
+    fun part2(input: List<String>) = Day09(input.single()).countGarbage()
 
-    chkTestInput(Part1, testInput, 0) { part1(it) }
+    chkTestInput(Part1, testInput, 3) { part1(it) }
     solve(Part1, input) { part1(it) }
 
-    chkTestInput(Part2, testInput, 0) { part2(it) }
+    chkTestInput(Part2, testInput, 17) { part2(it) }
     solve(Part2, input) { part2(it) }
+}
+
+private data class Day09(val input: String) {
+    private fun String.removeGarbage() = this.replace("!.".toRegex(), "").replace("<[^>]*>".toRegex(), "")
+    fun countGarbage(): Int {
+        val re = "<([^>]*)>".toRegex()
+        return re.findAll(input.replace("!.".toRegex(), "")).sumOf { it.groupValues[1].length }
+    }
+
+    fun groupScore(): Int {
+        var score = 0
+        var points = 0
+        input.removeGarbage().replace(",", "").forEach {
+            if (it == '{') {
+                points++
+            } else {
+                score += (points--)
+            }
+        }
+        return score
+    }
 }
